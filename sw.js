41328fe25eb9536e31e265f9c369059152d52311
VektorWP <vektorwp@users.noreply.github.com>
2026-07-04 23:58:00 +0000

    v3: correct productlists endpoint from API docs, per-store stock filter (I min butikk), fix invisible fallback button (CSS specificity)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "pwrskann-v2";
+const CACHE = "pwrskann-v3";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", e => {
